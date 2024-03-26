16715752b25be84b0136839349b38a2a3082e0db
Jia Ying <jywong.2020@economics.smu.edu.sg>
2024-03-26 17:48:43 +0800

    update somemore
    update interfaces

--- microservices/python_services/form/interface.py
+++ microservices/python_services/form/interface.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='/static')
@@ -11,15 +11,44 @@ db = SQLAlchemy(app) #initialise a connection to the database
 
 @app.route("/apply",methods=['GET', 'POST'])  
 def apply():
-    return render_template("apply.html", image_file="images/image1.png")
+    if request.method == "POST":
+        return render_template("apply.html")
+    else:
+        return render_template("apply.html")
 
 @app.route("/label",methods=['GET', 'POST'])
 def label():
-    return render_template("label.html", image_file="images/image1.png")
+    return render_template("label.html")
 
 @app.route("/alternative",methods=['GET', 'POST'])
 def alternative():
-    return render_template("alternative.html", image_file="images/image1.png")
+    if request.method == "POST":
+        result = request.form['cNum']
+        return render_template("alternative.html", result = result)
+    else:
+        return render_template("alternative.html")
+    
+@app.route("/alternative/<string:caseNum>")
+def getCase(caseNum):
+    # book = db.session.scalars(
+    # 	db.select(Book).filter_by(isbn13=isbn13).
+    # 	limit(1)
+    #     ).first()
+
+    # if book:
+    #     return jsonify(
+    #         {
+    #             "code": 200,
+    #             "data": book.json()
+    #         }
+    #     )
+    # return jsonify(
+    #     {
+    #         "code": 404,
+    #         "message": "Book not found."
+    #     }
+    # ), 404
+    return render_template("apply.html")
 
 if __name__ == "__main__":
     app.run(port = 5000, debug = True)
